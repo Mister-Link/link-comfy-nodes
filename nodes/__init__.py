c6a4108e8ac9ced6e1feda3c4e2d4557ebd1b3d9
Mister Link <link.ray@usa.com>
2026-01-01 04:40:28 -0700

    More fixes on auto cropper

--- nodes/__init__.py
+++ nodes/__init__.py
@@ -1,5 +1,6 @@
 """Node implementations grouped by domain."""
 
+from .auto_cropper import AutoCropperNode
 from .color_nodes import ColorParserNode, FarthestColorNode
 from .image_nodes import (
     CropToContentNode,
@@ -12,7 +13,6 @@ from .image_nodes import (
 from .pixel_art.node import ConvertToPixelArt
 from .string_nodes import AdvancedStringConcat, PreviewAsMarkdown, SaveFolderAsZip
 from .video_nodes import (
-    AutoCropperNode,
     BatchImageSave,
     PreviewImageAlpha,
     ReplaceAlpha,
