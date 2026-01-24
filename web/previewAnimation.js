7805541dba1b7ffcae7efc265f432b0648f6952c
Mister Link <link.ray@usa.com>
2026-01-24 12:08:24 -0700

    Update previewAnimation.js

--- web/previewAnimation.js
+++ web/previewAnimation.js
@@ -57,7 +57,10 @@ app.registerExtension({
             });
           }
 
-          if (this._previewAnimationVideos && this._previewAnimationVideos.length) {
+          if (
+            this._previewAnimationVideos &&
+            this._previewAnimationVideos.length
+          ) {
             this._previewAnimationVideos.forEach((video) => {
               video.pause();
               video.src = "";
@@ -82,136 +85,137 @@ app.registerExtension({
           const wrapper = document.createElement("div");
           wrapper.className = "lc-preview-animation-wrapper";
 
-            const videoElement = document.createElement("video");
-            videoElement.className = "lc-preview-animation-video";
-            videoElement.autoplay = true;
-            videoElement.loop = true;
-            videoElement.muted = true;
-            videoElement.playsInline = true;
-            videoElement.controls = false;
+          const videoElement = document.createElement("video");
+          videoElement.className = "lc-preview-animation-video";
+          videoElement.autoplay = true;
+          videoElement.loop = true;
+          videoElement.muted = true;
+          videoElement.playsInline = true;
+          videoElement.controls = false;
 
-            wrapper.appendChild(videoElement);
+          wrapper.appendChild(videoElement);
 
-            const widget = this.addDOMWidget(
-              "preview_" + i,
-              "preview_animation",
-              wrapper,
-              { serialize: false }
-            );
+          const widget = this.addDOMWidget(
+            "preview_" + i,
+            "preview_animation",
+            wrapper,
+            { serialize: false },
+          );
 
-            widget.videoElement = videoElement;
-            widget.computeSize = function (width) {
-              if (this.videoElement && this.videoElement.videoWidth > 0) {
-                const aspectRatio =
-                  this.videoElement.videoHeight / this.videoElement.videoWidth;
-                const previewWidth = width - 20;
-                const previewHeight = previewWidth * aspectRatio;
-                return [width, previewHeight + 20];
-              }
-              return [width, 220];
-            };
-            this._previewAnimationVideos.push(videoElement);
+          widget.videoElement = videoElement;
+          widget.computeSize = function (width) {
+            if (this.videoElement && this.videoElement.videoWidth > 0) {
+              const aspectRatio =
+                this.videoElement.videoHeight / this.videoElement.videoWidth;
+              const previewWidth = width - 20;
+              const previewHeight = previewWidth * aspectRatio;
+              return [width, previewHeight + 20];
+            }
+            return [width, 220];
+          };
+          this._previewAnimationVideos.push(videoElement);
 
-            // Build the URL for the preview
-            const params = new URLSearchParams({
-              filename: gif.filename,
-              type: gif.type,
-              subfolder: gif.subfolder || "",
-            });
+          // Build the URL for the preview
+          const params = new URLSearchParams({
+            filename: gif.filename,
+            type: gif.type,
+            subfolder: gif.subfolder || "",
+          });
 
-            const url = api.apiURL(`/view?${params.toString()}`);
+          const url = api.apiURL(`/view?${params.toString()}`);
 
-            // Load the video
-            videoElement.src = url;
-            videoElement.load();
+          // Load the video
+          videoElement.src = url;
+          videoElement.load();
 
-            // Play when loaded
-            videoElement.addEventListener("loadeddata", () => {
-              videoElement
-                .play()
-                .catch((e) => console.log("Auto-play failed:", e));
-              const nodeWidth =
-                this.size && this.size[0] ? this.size[0] : 240;
-              const nodeHeight =
-                this.size && this.size[1] ? this.size[1] : 0;
-              const newSize = widget.computeSize(nodeWidth);
-              if (nodeHeight < newSize[1]) {
-                this.setSize([nodeWidth, newSize[1]]);
-              }
-              this.setDirtyCanvas(true, true);
-            });
+          // Play when loaded
+          videoElement.addEventListener("loadeddata", () => {
+            videoElement
+              .play()
+              .catch((e) => console.log("Auto-play failed:", e));
+            const nodeWidth = this.size && this.size[0] ? this.size[0] : 240;
+            const nodeHeight = this.size && this.size[1] ? this.size[1] : 0;
+            const newSize = widget.computeSize(nodeWidth);
+            if (nodeHeight < newSize[1]) {
+              this.setSize([nodeWidth, newSize[1]]);
+            }
+            this.setDirtyCanvas(true, true);
+          });
 
-            // Clean up on widget removal
-            const originalOnRemove = this.onRemoved;
-            this.onRemoved = function () {
-              if (this._previewAnimationVideos && this._previewAnimationVideos.length) {
-                this._previewAnimationVideos.forEach((video) => {
-                  video.pause();
-                  video.src = "";
-                  video.remove();
-                });
-                this._previewAnimationVideos = [];
-              }
-              if (originalOnRemove) {
-                originalOnRemove.apply(this, arguments);
-              }
-            };
+          // Clean up on widget removal
+          const originalOnRemove = this.onRemoved;
+          this.onRemoved = function () {
+            if (
+              this._previewAnimationVideos &&
+              this._previewAnimationVideos.length
+            ) {
+              this._previewAnimationVideos.forEach((video) => {
+                video.pause();
+                video.src = "";
+                video.remove();
+              });
+              this._previewAnimationVideos = [];
+            }
+            if (originalOnRemove) {
+              originalOnRemove.apply(this, arguments);
+            }
+          };
 
-            // Add context menu options
-            const originalGetExtraMenuOptions = this.getExtraMenuOptions;
+          // Add context menu options
+          const originalGetExtraMenuOptions = this.getExtraMenuOptions;
           this.getExtraMenuOptions = function (_, options) {
             if (originalGetExtraMenuOptions) {
               originalGetExtraMenuOptions.apply(this, arguments);
             }
 
-              // Add video-specific options
-              options.unshift(
-                {
-                  content: "Open preview",
-                  callback: () => {
-                    window.open(url, "_blank");
-                  },
+            // Add video-specific options
+            options.unshift(
+              {
+                content: "Open preview",
+                callback: () => {
+                  window.open(url, "_blank");
                 },
-                {
-                  content: "Save preview",
-                  callback: () => {
-                    const a = document.createElement("a");
-                    a.href = url;
-                    a.download = gif.filename;
-                    document.body.appendChild(a);
-                    a.click();
-                    document.body.removeChild(a);
-                  },
+              },
+              {
+                content: "Save preview",
+                callback: () => {
+                  const a = document.createElement("a");
+                  a.href = url;
+                  a.download = gif.filename;
+                  document.body.appendChild(a);
+                  a.click();
+                  document.body.removeChild(a);
                 },
-                {
-                  content: "Copy output filepath",
-                  callback: () => {
-                    const filepath = `${gif.subfolder ? gif.subfolder + "/" : ""}${gif.filename}`;
-                    navigator.clipboard.writeText(filepath).then(() => {
-                      console.log("Copied to clipboard:", filepath);
-                    });
-                  },
+              },
+              {
+                content: "Copy output filepath",
+                callback: () => {
+                  const filepath = `${gif.subfolder ? gif.subfolder + "/" : ""}${gif.filename}`;
+                  navigator.clipboard.writeText(filepath).then(() => {
+                    console.log("Copied to clipboard:", filepath);
+                  });
                 },
-                null, // separator
-                {
-                  content: widget.videoElement.paused
-                    ? "Play preview"
-                    : "Pause preview",
-                  callback: () => {
-                    if (widget.videoElement.paused) {
-                      widget.videoElement.play();
-                    } else {
-                      widget.videoElement.pause();
-                    }
-                  },
+              },
+              null, // separator
+              {
+                content: widget.videoElement.paused
+                  ? "Play preview"
+                  : "Pause preview",
+                callback: () => {
+                  if (widget.videoElement.paused) {
+                    widget.videoElement.play();
+                  } else {
+                    widget.videoElement.pause();
+                  }
                 },
-                {
-                  content: "Mute Preview",
-                  callback: () => {
-                    widget.videoElement.muted = !widget.videoElement.muted;
-                  },
+              },
+              {
+                content: "Mute Preview",
+                callback: () => {
+                  widget.videoElement.muted = !widget.videoElement.muted;
                 },
-                null // separator
+              },
+              null, // separator
             );
           };
         }
@@ -223,8 +227,7 @@ app.registerExtension({
           this.setSize([nodeWidth, minNodeHeight]);
         }
         this.setDirtyCanvas(true, true);
-      }
       };
     }
   },
-  });
+});
